2d0822d129e74c625a50cbf2f0b71bab2a1e4316
azamov-asadbek271 <azamjonovasa45@gmail.com>
2024-06-12 07:36:48 +0300

    qqq

---
+++ src/app/about/page.jsx
@@ -0,0 +1,26 @@
+function About() {
+  return (
+    <div className="flex w-full flex-col mx-auto max-w-6xl mt-20">
+      <div>
+        <h2 className="font-bold text-2xl">O'zim haqimda</h2>
+        <p className="mt-6">
+          Azamov Asadbek 2000 yil Fargona Viloati Margilon shahrida
+          tugulganman, 2022 yil oxirlarida Frontend yonalishi boyicha
+          oqishni boshlagann,Codial akadmiyasi va Najot talimni Frontend
+          yonalishi boyicha bitirganman.Odamlar bilan tezda chiqishishga
+          harakat qilaman hamma joyda lider bolishga harakat qilaman va xatolar
+          togrlashni yaxshi koraman.
+        </p>
+        <h2 className="font-bold text-2xl mt-10">Qiziqishlarim</h2>
+        <p className="mt-6">Fudbol oynashni va korishni yaxshi koraman.</p>
+        <h2 className="font-bold text-2xl mt-10">Maqsadim</h2>
+        <p className="mt-6">
+          Yaxshi Inson Bolish va Odamlarga foydam tegishi albatta yaxshi pul
+          topish maqsadim
+        </p>
+      </div>
+    </div>
+  );
+}
+
+export default About;
